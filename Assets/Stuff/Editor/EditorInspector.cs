2c0c71683ddcaa448a7e4419c9f33df2b2019230
seldomU <seldom.indeed@gmail.com>
2015-03-03 05:30:43 +0100

    adding editor inspector and another tween test window

---
+++ Assets/Stuff/Editor/EditorInspector.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public class EditorInspector : EditorWindow
+{
+	int selectedWindowId;
+	Vector2 inspectorScrollPos;
+
+	void OnGUI()
+	{
+		// get the windows
+		var windows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+
+		// pick one
+		EditorGUILayout.Space();
+		selectedWindowId = EditorGUILayout.Popup("Inspect window", selectedWindowId, windows.Select(w => w.title).ToArray());
+
+		// get an inspector for it
+		var inspectorEditor = Editor.CreateEditor(windows[selectedWindowId]);
+
+		// draw the inspector
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Inspector", EditorStyles.boldLabel);
+		inspectorScrollPos = GUILayout.BeginScrollView(inspectorScrollPos);
+		inspectorEditor.OnInspectorGUI();
+		GUILayout.EndScrollView();
+	}
+
+	[MenuItem("Window/Editor inspector")]
+	static void ShowWindow() { GetWindow<EditorInspector>(); }
+}
+
---
+++ Assets/Stuff/Editor/TweenCircleTest.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using RelationsInspector;
+
+public class TweenCircleTest : EditorWindow
+{
+	// inspectable variables
+	public Vector2 center;
+	public float radius = 50;
+	public int numTargets = 10;
+	public float updateTargetInterval = 0.1f;
+	public float tweenDuration = 0.4f;
+
+	float lastTargetSetTime;
+	int lastTargetSet;
+
+	RectObj rectObj;
+	Vector2[] targets;
+
+	Editor editor;
+
+	void OnEnable()
+	{
+		rectObj = new RectObj(new Rect(center.x, center.y, 5, 5));
+	}
+
+	void Restart()
+	{
+		targets = new Vector2[numTargets];
+		for (int i = 0; i < numTargets; i++)
+		{
+			float frac = i / (float)numTargets;
+			targets[i] = new Vector2(Mathf.Cos(frac * (2 * Mathf.PI)), Mathf.Sin(frac * (2 * Mathf.PI)));
+			targets[i] *= radius;
+			targets[i] += center;
+		}
+		rectObj = new RectObj(new Rect(center.x, center.y, 5, 5));
+
+		lastTargetSetTime = 0;
+		lastTargetSet = -1;
+	}
+
+	void OnGUI()
+	{
+		EditorGUI.DrawRect(rectObj.rect, Color.red);
+
+		// draw targets
+		if (targets != null)
+		{
+			for (int i = 0; i < targets.Length; i++)
+			{
+				EditorGUI.DrawRect(new Rect(targets[i].x, targets[i].y, 2, 2), Color.yellow);
+			}
+		}
+
+
+		if (Event.current.type == EventType.mouseDown)
+		{
+			Restart();
+			// tween the rectObj
+			//Tweener.MoveRectTo(rectObj, Event.current.mousePosition, 1f);
+		}
+	}
+
+	void Update()
+	{
+		if (targets != null && lastTargetSet + 1 < targets.Length)
+		{
+			double time = EditorApplication.timeSinceStartup;
+			if (time >= lastTargetSetTime + updateTargetInterval)
+			{
+				int targetID = lastTargetSet + 1;
+				Tweener.gen.MoveRectTo(rectObj, targets[targetID], tweenDuration);
+
+				lastTargetSetTime = (float)time;
+				lastTargetSet = targetID;
+			}
+		}
+		Repaint();
+	}
+
+	
+	[MenuItem("Window/tweenCircleTest")]
+	static void Spawn()
+	{
+		GetWindow<TweenCircleTest>();
+	}
+}
