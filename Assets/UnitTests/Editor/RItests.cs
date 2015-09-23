d7546f70cd04d96568b952344cb577b107365b90
uwe <seldom.indeed@gmail.com>
2015-09-23 04:55:10 +0200

    updated ignore list, added unit test file

---
+++ Assets/UnitTests/Editor/RItests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Assets.UnitTests.Editor
+{
+    [TestFixture]
+    class RItests
+    {
+        [Test]
+        public void TestLayoutSaving()
+        {
+            //RelationsInspector.GraphPosSerialization.LoadGraphLayout()
+        }
+    }
+}
