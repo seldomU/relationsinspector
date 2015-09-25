961125279cfe1a3f379f45958a2890eb654315ec
uwe <seldom.indeed@gmail.com>
2015-09-25 13:14:08 +0200

    forgot a file

---
+++ RIDLLProject/Backend/IGraphBackendInternal.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RelationsInspector
+{
+    internal interface IGraphBackendInternal<T, P> where T : class
+    {
+        System.Type GetDecoratedType();
+        // initialize the backend object
+        IEnumerable<T> Init(IEnumerable<object> targets, RelationsInspectorAPI api);
+        IEnumerable<Tuple<T, P>> GetRelated(T entity);
+        IEnumerable<Tuple<T, P>> GetRelating(T entity);
+        void CreateEntity(Vector2 position);
+        void CreateRelation(T source, T target, P tag);
+        void OnEntitySelectionChange(T[] selection);
+        void OnUnitySelectionChange();
+        Rect OnGUI();
+
+        string GetEntityTooltip(T entity);
+        string GetTagTooltip(P tag);
+        Rect DrawContent(T entity, EntityDrawContext drawContext);
+
+        void OnEntityContextClick(IEnumerable<T> entities);
+        void OnRelationContextClick(T source, T target, P tag);
+        Color GetRelationColor(P relationTagValue);
+        void OnEvent(Event e);
+    }
+}
