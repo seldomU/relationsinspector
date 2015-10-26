50e7068e50b61b830625daa8d577943a304ef411
uwe <seldom.indeed@gmail.com>
2015-10-26 04:16:14 +0100

    added build batch files

---
+++ buildDemo.bat
@@ -0,0 +1,2 @@
+REM the fist quote is the command window title
+start "" "C:\Program Files (x86)\Unity4.6.1\Editor\Unity.exe" -batchmode -projectPath I:\code\RelationsInspector -executeMethod ExportPackage.DoExportDemoPackage -quit
