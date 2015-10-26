9242904e10c67c4d85f5fbd3fee20e4099779e05
uwe <seldom.indeed@gmail.com>
2015-10-26 04:15:55 +0100

    packages exclude settings and skin files

--- Assets/Stuff/Editor/ExportPackage.cs
+++ Assets/Stuff/Editor/ExportPackage.cs
@@ -6,53 +6,35 @@ using System.IO;
 
 public class ExportPackage
 {
-	[MenuItem("Window/Build/Export package")]
+    // don't include these in builds
+    static string[] excludePaths = new[] { SkinManager.LightSkinPath, SkinManager.DarkSkinPath, Settings.SettingsPath };
+
+    [MenuItem("Window/Build/Export package")]
 	public static void DoExportPackage()
 	{
-		string[] includedFilePaths = new[] { @"Assets\RelationsInspector", @"Assets\Editor Default Resources" };
-		string packagePath = "RelationsInspector.unitypackage";
-
-		PreparePackageExport();	// remove files not intended for distribution
-		AssetDatabase.ExportPackage(includedFilePaths, packagePath, ExportPackageOptions.Recurse);
+        RunBuild( "RelationsInspector.unitypackage" );
 	}
 
 	[MenuItem("Window/Build/Export demo package")]
 	public static void DoExportDemoPackage()
 	{
-		string[] includedFilePaths = new[] { @"Assets\RelationsInspector", @"Assets\Editor Default Resources" };
-		string packagePath = "RelationsInspectorDemo.unitypackage";
+        RunBuild( "RelationsInspectorDemo.unitypackage" );
+    }
 
-		PreparePackageExport();	// remove files not intended for distribution
-		AssetDatabase.ExportPackage(includedFilePaths, packagePath, ExportPackageOptions.Recurse);
-	}
+    static void RunBuild(string packageName)
+    {
+        string[] includedFilePaths = new[] { @"Assets\RelationsInspector", @"Assets\Editor Default Resources" };
 
-	/*
-	[MenuItem("Window/Build/Prepare export")]
-	public static void PrepareGit()
+        PreparePackageExport(); // remove files not intended for distribution
+        AssetDatabase.ExportPackage( includedFilePaths, packageName, ExportPackageOptions.Recurse );
+        CleanupPackageExport();
+
+    }
+
+    [MenuItem( "Window/Build/RunPrep" )]    // menuitem for testing
+    static void PreparePackageExport()
 	{
-		MoveGitFiles(toTemp: true);
-	}
-
-	[MenuItem("Window/Build/Cleanup export")]
-	public static void CleanUpGit()
-	{
-		MoveGitFiles(toTemp: false);
-	}
-
-	static void MoveGitFiles(bool toTemp)
-	{
-		var utilsDir = Application.dataPath + @"\RelationsInspector\Editor\BackendUtils";
-		var tempDir = Application.dataPath + @"\TempGitFiles";
-		var fileNames = new[] { ".git", ".gitignore", "LICENSE", "LICENSE.meta", "README.md", "README.md.meta" };
-		var sourceDir = toTemp ? utilsDir : tempDir;
-		var targetDir = toTemp ? tempDir : utilsDir;
-		foreach (var fileName in fileNames)
-			File.Move(Path.Combine(sourceDir, fileName), Path.Combine(targetDir, fileName));
-	}*/
-
-	static void PreparePackageExport()
-	{
-		// close all window instances (so that we can remove the config)
+        // close all window instances (so that we can remove the config)
 		var windows = Resources.FindObjectsOfTypeAll<RelationsInspectorWindow>();
 		for (int i = 0; i < windows.Length; i++)
 			windows[i].Close();
@@ -60,5 +42,21 @@ public class ExportPackage
 		// remove debug symbol files
 		AssetDatabase.DeleteAsset(@"Assets\RelationsInspector\Editor\RelationsInspector.dll.mdb");
 		AssetDatabase.DeleteAsset(@"Assets\RelationsInspector\Editor\RelationsInspector.pdb");
+
+        
+        foreach ( var path in excludePaths )
+            AssetDatabase.MoveAsset( path, GetTempPath( path ) );
 	}
+
+    [MenuItem( "Window/Build/RunCleanup" )]     // menuitem for testing
+    static void CleanupPackageExport()
+    {
+        foreach ( var path in excludePaths )
+            AssetDatabase.MoveAsset( GetTempPath( path ), path );
+    }
+
+    static string GetTempPath( string path )
+    {
+        return Path.Combine( @"Assets\Stuff", Path.GetFileName( path ) );
+    }
 }
