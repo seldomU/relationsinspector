9b6a1ece5fbf4c190631decdae45bd3f784ed714
uwe <seldom.indeed@gmail.com>
2016-01-21 13:05:32 +0100

    fixed RI name in menu item

--- Assets/RelationsInspector/Editor/RelationsInspectorMenuItem.cs
+++ Assets/RelationsInspector/Editor/RelationsInspectorMenuItem.cs
@@ -3,7 +3,7 @@ using RelationsInspector;
 
 public class RelationsInspectorMenuItem
 {
-	[MenuItem("Window/Relations Inspector")]
+	[MenuItem("Window/RelationsInspector")]
 	static void SpawnWindow()
 	{
 		EditorWindow.GetWindow<RelationsInspectorWindow>("Relations", typeof(SceneView));
