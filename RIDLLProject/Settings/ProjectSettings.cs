c7f81bb75f4371db138a50c9ed27102e3fde503c
uwe <seldom.indeed@gmail.com>
2016-02-02 17:09:16 +0100

    changed Manual and discussion URLs

--- RIDLLProject/Settings/ProjectSettings.cs
+++ RIDLLProject/Settings/ProjectSettings.cs
@@ -19,8 +19,8 @@ namespace RelationsInspector
 		const string lightSkinName = "RIWindowLightSkin.asset";
 		const string SettingsFileName = "Settings.asset";
 
-		internal const string DocURL = @"https://github.com/seldomU/RIBackendUtil/wiki";
-		internal const string DiscussionURL = @"http://forum.unity3d.com/threads/demo-relations-inspector-asset.295319/";
+		internal const string DocURL = @"https://github.com/seldomU/RIBackendUtil/wiki/RelationsInspector-Manual";
+		internal const string DiscussionURL = @"http://forum.unity3d.com/threads/relationsinspector-reveal-structures-in-your-project-demo.382792/";
 		internal const string StoreURL = @"content/53147";
 
 		public static string[] obligatoryFileNames = new[]
