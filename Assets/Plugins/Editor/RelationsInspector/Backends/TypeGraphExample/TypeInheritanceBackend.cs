89ec3a42a623fb9a40dd8e2f9e04cf7923d7d51f
uwe <seldom.indeed@gmail.com>
2016-03-08 06:15:03 +0100

    TypeInheritanceBackend default depth is 2

--- Assets/Plugins/Editor/RelationsInspector/Backends/TypeGraphExample/TypeInheritanceBackend.cs
+++ Assets/Plugins/Editor/RelationsInspector/Backends/TypeGraphExample/TypeInheritanceBackend.cs
@@ -21,7 +21,20 @@ namespace RelationsInspector.Backend.TypeHierarchy
 
 		// maintain the target types and their parents, so we can hide the other types derived from the parents
 		HashSet<Type> targetTypes = new HashSet<Type>();
-		HashSet<Type> parentTypes = new HashSet<Type>();
+
+		Dictionary<Type, int> touchedSubTypes = new Dictionary<Type, int>();
+		Dictionary<Type, int> touchedSuperTypes = new Dictionary<Type, int>();
+		static int subTypeDepth;
+		const string subTypeDepthKey = "TypeInheritanceBackend" + "subTypeDepth";
+		static int superTypeDepth;
+		const string superTypeDepthKey = "TypeInheritanceBackend" + "superTypeDepth";
+
+		public override void Awake( GetAPI getAPI )
+		{
+			subTypeDepth = EditorPrefs.GetInt( subTypeDepthKey, 2 );
+			superTypeDepth = EditorPrefs.GetInt( superTypeDepthKey, 2 );
+			base.Awake( getAPI );
+		}
 
 		public override IEnumerable<Type> Init( object target )
 		{
@@ -38,25 +51,40 @@ namespace RelationsInspector.Backend.TypeHierarchy
 		// returns relations entity->subtype, unless entity is the parent of a target
 		IEnumerable<Relation<Type, TypeRelation>> GetRelated( Type entity )
 		{
-			if ( parentTypes.Contains( entity ) )
+			// for superTypes of the targets, don't return subtypes
+			if ( touchedSuperTypes.Keys.Contains( entity ) )
+				yield break;
+
+			if ( entity.IsInterface )
+				yield break;
+
+			int depth = touchedSubTypes.ContainsKey( entity ) ? touchedSubTypes[ entity ] + 1 : 1;
+			if ( depth > subTypeDepth )
 				yield break;
 
 			var subTypes = TypeUtility.GetSubtypes( entity, allAssemblies );
 			foreach ( var t in subTypes )
+			{
+				touchedSubTypes[ t ] = touchedSubTypes.ContainsKey( t ) ? Math.Min( depth, touchedSubTypes[ t ] ) : depth;
 				yield return GetRelation( entity, t );
+			}
 		}
 
 		// returns relations baseType->entity and interface->entity
 		IEnumerable<Relation<Type, TypeRelation>> GetRelating( Type entity )
 		{
-			bool isParentOrTarget = parentTypes.Contains( entity ) || targetTypes.Contains( entity );
+			bool isParentOrTarget = touchedSuperTypes.ContainsKey( entity ) || targetTypes.Contains( entity );
+
+			int depth = touchedSuperTypes.ContainsKey( entity ) ? touchedSuperTypes[ entity ] + 1 : 1;
+			if ( depth > superTypeDepth )
+				yield break;
 
 			if ( entity.BaseType != null && !entity.BaseType.IsInterface )
 			{
 				yield return GetRelation( entity.BaseType, entity );
 
 				if ( isParentOrTarget )
-					parentTypes.Add( entity.BaseType );
+					touchedSuperTypes[ entity.BaseType ] = depth;
 			}
 
 			if ( includeInterfaces )
@@ -66,15 +94,15 @@ namespace RelationsInspector.Backend.TypeHierarchy
 					yield return GetRelation( t, entity );
 
 					if ( isParentOrTarget )
-						parentTypes.Add( t );
+						touchedSuperTypes[ entity.BaseType ] = depth;
 				}
 			}
 		}
 
 		static Relation<Type, TypeRelation> GetRelation( Type sourceType, Type targetType )
 		{
-			var relationType = sourceType.IsInterface ? 
-				(targetType.IsInterface ? TypeRelation.Extension : TypeRelation.Implementation) : 
+			var relationType = sourceType.IsInterface ?
+				( targetType.IsInterface ? TypeRelation.Extension : TypeRelation.Implementation ) :
 				TypeRelation.Extension;
 			return new Relation<Type, TypeRelation>( sourceType, targetType, relationType );
 		}
@@ -91,9 +119,22 @@ namespace RelationsInspector.Backend.TypeHierarchy
 				if ( EditorGUI.EndChangeCheck() )
 					api.Rebuild();
 
-				GUILayout.FlexibleSpace();
-				if ( !api.GetTargets().Any() && GUILayout.Button( "inspect Unity Object" ) )
-					api.ResetTargets( new[] { typeof( UnityEngine.Object ) } );
+				EditorGUI.BeginChangeCheck();
+				GUILayout.Space( 20 );
+				GUILayout.Label( "Subtype depth", EditorStyles.miniLabel );
+				//EditorGUIUtility.labelWidth = 110;
+				subTypeDepth = EditorGUILayout.IntField( subTypeDepth, GUILayout.Width( 35 ) );
+				GUILayout.Space( 20 );
+				//EditorGUIUtility.labelWidth = 110;
+				GUILayout.Label( "Supertype depth", EditorStyles.miniLabel );
+				superTypeDepth = EditorGUILayout.IntField( superTypeDepth, GUILayout.Width(35) );
+				if ( EditorGUI.EndChangeCheck() )
+				{
+					EditorPrefs.SetInt( subTypeDepthKey, subTypeDepth );
+					EditorPrefs.SetInt( superTypeDepthKey, superTypeDepth );
+					api.Rebuild();
+					api.Relayout();
+				}
 
 				// search field
 				GUILayout.FlexibleSpace();
@@ -109,11 +150,11 @@ namespace RelationsInspector.Backend.TypeHierarchy
 			if ( entities.Count() == 1 )
 			{
 				var single = entities.First();
-				if( !api.GetTargets().Contains( single ) )
+				if ( !api.GetTargets().Contains( single ) )
 					menu.AddItem( new GUIContent( "inspect type " + single.Name ), false, () => api.ResetTargets( new[] { single } ) );
 
 				var typeAsset = behaviourTypeAssets.FirstOrDefault( monoScript => monoScript.GetClass() == single );
-				if( typeAsset != null )
+				if ( typeAsset != null )
 					menu.AddItem( new GUIContent( "open " + single.Name ), false, () => AssetDatabase.OpenAsset( typeAsset ) );
 				// UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal
 			}
