ad75b9950d40ada47a1ed03517328b889eed1077
uwe <seldom.indeed@gmail.com>
2016-03-10 04:15:09 +0100

    changed version to 1.1

--- RIDLLProject/AssemblyInfo.cs
+++ RIDLLProject/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "seldomU" )]
 [assembly: AssemblyProduct( "RelationsInspector" )]
-[assembly: AssemblyCopyright( "Copyright ©  2015" )]
+[assembly: AssemblyCopyright( "Copyright ©  2016" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.0.*" )]
+[assembly: AssemblyVersion( "1.1.0" )]
 //[assembly: AssemblyFileVersion( "1.0.0.0" )]
